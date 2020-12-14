c00bdce9819930ec9083b046b40c3b13b3174ed4
Gustavo <gblanco@gmail.com>
2020-12-13 19:39:31 -0500

    third commit

--- lab12Django/prestamo/urls.py
+++ lab12Django/prestamo/urls.py
@@ -6,6 +6,4 @@ urlpatterns = [
     path('prestamo/crear', views.crear),
     path('autor/<int:autor_id>', views.libros_autor),
     path('prestamo/guardar', views.guardar),
-    # url(r'^series/$', views.serie_list),
-    # url(r'^series/(?P<pk>[0-9]+)/$', views.serie_detail),
 ]
